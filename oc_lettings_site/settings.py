2aac54e0be7fd8f9706d685f292cf18f164d696d
Anna Kaczorowski <kaczoro.anna@gmail.com>
2024-01-02 15:26:46 +0100

    create new django app 'profiles'

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lettings',
+    'profiles',
     'oc_lettings_site.apps.OCLettingsSiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
