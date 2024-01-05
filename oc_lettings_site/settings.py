752dd908d1b0e303e67b22a9658e0c779550988e
Anna Kaczorowski <kaczoro.anna@gmail.com>
2024-01-05 16:43:28 +0100

    update settings to allow all hosts (testing deployment)

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,12 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'  # temporary !
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  # temporary !
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']  # temporary, to be changed for production setup
+ALLOWED_HOSTS = ['*']  # temporary !
 
 # Application definition
 
