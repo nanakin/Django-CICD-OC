c7fdec9f4ae9ddd157329977ff725c20d9339788
Anna Kaczorowski <kaczoro.anna@gmail.com>
2024-01-16 22:09:24 +0100

    add testing for custom 404 and 500 templates

--- oc_lettings_site/tests/test_views.py
+++ oc_lettings_site/tests/test_views.py
@@ -1,3 +1,4 @@
+import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
 
@@ -9,6 +10,22 @@ def test_index_view(client):
     assert "Welcome to Holiday Homes" in content
     assert response.status_code == 200
     assertTemplateUsed(response, "oc_lettings_site/index.html")
+
+
+def test_404_view(client):
+    response = client.get("/invalidurl/")
+    assert response.status_code == 404
+    assert "Page not found" in response.content.decode()
+    assertTemplateUsed(response, "404.html")
+
+
+@pytest.mark.django_db
+def test_500_view(client):
+    client.raise_request_exception = False
+    response = client.get("/profiles/invalidusername/")
+    assert response.status_code == 500
+    assert "Internal Server Error" in response.content.decode()
+    assertTemplateUsed(response, "500.html")
 
 
 def test_admin_view(client):
